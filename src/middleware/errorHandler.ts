7da4dc2a8be957a5005357dc8c695e6d88b5e7fc
Ludvig Möller <Ludvig.Moller@outlook.com>
2026-09-08 19:15:47 +0200

    feat: add errorHandler

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,10 +1,19 @@
 import { NextFunction, Request, Response } from "express";
+import { HttpError } from "../errors/HttpError";
 
 export function errorHandler(
     err: Error, 
-    req: Request, 
+    _req: Request, 
     res: Response, 
-    next: NextFunction,
+    _next: NextFunction,
 ) {
+    if (err instanceof HttpError) {
+        return res.status(err.statusCode).json({
+            error: err.message
+        });
+    }
 
+    return res.status(500).json({
+        error: "Internal server error"
+    });
 }
